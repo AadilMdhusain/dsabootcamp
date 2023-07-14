3ee17062c0006fbb4e31afe69b38cedff3c9edf8
Aadil <aadilmohammadhusain@gmail.com>
2023-07-14 22:52:23 +0530

    first commit

---
+++ work.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[5][5], i, j;
+    cout << "The address of the array variable \"arr\" " << &arr << endl;
+    cout << endl;
+    for (i = 0; i < 5; i++)
+    {
+        cout << "The address of the array elements " << &arr[i] << endl;
+    }
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            cout << "The address of the array elements " << &arr[i][j] << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
