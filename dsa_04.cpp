76c70536162c6454268eee406de4f00b1d9611fb
Ebadulislam1001 <ebadulislamfarooqi@gmail.com>
2023-09-08 19:28:03 +0530

    minor fixes

--- dsa_04.cpp
+++ dsa_04.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int arr[5][5],i,j;
-    cout<<"The address of the array variable \"arr\" "<<&arr<<endl;
-    cout<<endl;
-    for(i=0;i<5;i++)
+    int arr[5][5], i, j;
+    cout << "The address of the array variable \"arr\" " << &arr << endl;
+    cout << endl;
+    for (i = 0; i < 5; i++)
     {
-        cout<<"The address of the array elements present in the first row  "<<&arr[i]<<endl;
-    }  
-    cout<<endl;
-    cout<<endl;
-    cout<<"The address of all the elements of the array row-wise"<<endl<<endl;
-    for(i=0;i<5;i++)
+        cout << "The address of the array elements present in the first row  " << &arr[i] << endl;
+    }
+    cout << endl;
+    cout << endl;
+    cout << "The address of all the elements of the array row-wise" << endl
+         << endl;
+    for (i = 0; i < 5; i++)
     {
-        for(j=0;j<5;j++)
+        for (j = 0; j < 5; j++)
         {
-            cout<<"The address of the array elements "<<&arr[i][j]<<endl;
+            cout << "The address of the array elements " << &arr[i][j] << endl;
         }
-        cout<<endl;
+        cout << endl;
     }
 
-   return 0;
+    return 0;
 }
--- dsa_08.cpp
+++ dsa_08.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 struct node
@@ -10,34 +9,33 @@ struct node
 
 int main()
 {
-    struct node *letter1,*letter2,*letter3,*letter4,*letter5;
-    letter1 = (struct node*)malloc(sizeof(struct node));
-    letter2 = (struct node*)malloc(sizeof(struct node));
-    letter3 = (struct node*)malloc(sizeof(struct node));
-    letter4 = (struct node*)malloc(sizeof(struct node));
-    letter5 = (struct node*)malloc(sizeof(struct node));
+    struct node *letter1, *letter2, *letter3, *letter4, *letter5;
+    letter1 = (struct node *)malloc(sizeof(struct node));
+    letter2 = (struct node *)malloc(sizeof(struct node));
+    letter3 = (struct node *)malloc(sizeof(struct node));
+    letter4 = (struct node *)malloc(sizeof(struct node));
+    letter5 = (struct node *)malloc(sizeof(struct node));
 
-    letter1->x='H';
-    letter2->x='E';
-    letter3->x='L';
-    letter4->x='L';
-    letter5->x='O';
+    letter1->x = 'H';
+    letter2->x = 'E';
+    letter3->x = 'L';
+    letter4->x = 'L';
+    letter5->x = 'O';
 
-    letter1->next=letter2;
-    letter2->next=letter3;
-    letter3->next=letter4;
-    letter4->next=letter5;
-    letter5->next=NULL;
-    cout<<"The word represented by the Structure is "<<endl;
+    letter1->next = letter2;
+    letter2->next = letter3;
+    letter3->next = letter4;
+    letter4->next = letter5;
+    letter5->next = NULL;
+    cout << "The word represented by the Structure is " << endl;
 
     struct node *temp;
-    temp=letter1;
-    while(temp!=NULL)
+    temp = letter1;
+    while (temp != NULL)
     {
-        cout<<temp->x;
-        temp=temp->next;
+        cout << temp->x;
+        temp = temp->next;
     }
-
 
     return 0;
 }
